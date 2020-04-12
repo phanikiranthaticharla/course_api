2f1c303854b7d031b6b772ae6c3a765c5ce04826
phanikiranthaticharla <t.phani.kiran@gmail.com>
2020-04-12 01:02:41 -0700

    Added support for adding a new topic using postmapping

--- src/main/java/com/phani/springbootquickstart/course_api/topic/TopicController.java
+++ src/main/java/com/phani/springbootquickstart/course_api/topic/TopicController.java
@@ -2,7 +2,10 @@ package com.phani.springbootquickstart.course_api.topic;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,9 +15,20 @@ public class TopicController {
 	private TopicService topicService; 
 	
 	
-	@RequestMapping("/topics")
+	@GetMapping("/topics")
 	public List<Topic> listAllTopics() {
 		return topicService.getAllTopics();
 				
 	}
+	
+	@GetMapping("/topics/{topicId}")
+	public Topic getTopic(@PathVariable String topicId) {
+		return topicService.getTopic(topicId);
+	}
+	
+	@PostMapping("/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	}
+	
 }
--- src/main/java/com/phani/springbootquickstart/course_api/topic/TopicService.java
+++ src/main/java/com/phani/springbootquickstart/course_api/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.phani.springbootquickstart.course_api.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,16 +9,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 	
-	private List<Topic> topics =  Arrays.asList(
+	private List<Topic> topics =  new ArrayList<>(Arrays.asList(
 			new Topic("Hadoop", "Hadoop EcoSystem", "Hadoop Description"), 
 			new Topic("Jersey", "Java Jersey", "REST API using Jersey"), 
 			new Topic("Spring", "SpringBoot Framework", "REST API using SpringBoot Framework")
-			);
+			));
 
 	public List<Topic> getAllTopics() {
 		return topics; 
 		
 	} 
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+		
+	}
 	
 	
 }
