13372f9fe4d4cfde1a3d660a9ee12f0fcec7aa3d
phanikiranthaticharla <t.phani.kiran@gmail.com>
2020-04-12 01:21:36 -0700

    Add a test case verify individual topic

--- src/test/java/com/phani/springbootquickstart/course_api/CourseApiApplicationTests.java
+++ src/test/java/com/phani/springbootquickstart/course_api/CourseApiApplicationTests.java
@@ -9,6 +9,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -46,6 +48,45 @@ class CourseApiApplicationTests {
 			System.out.println("response "+response.toString());
 		} catch (IOException e) {
 			e.printStackTrace();
+		}	
+		
+		
+	}
+	
+	@Test
+	void verifyGETSingleTopic() {
+        
+		
+		try {
+			URL url = new URL("http://localhost:9090/topics/Hadoop"); 
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			int responseCode = connection.getResponseCode();
+
+			int expectedCode = 200; 
+			assertEquals(expectedCode, responseCode);
+			
+			BufferedReader in = new BufferedReader(
+					new InputStreamReader(connection.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			System.out.println("response "+response.toString());
+			JSONObject jsonObj = new JSONObject(); 
+       		
+		    jsonObj.put("name", "Hadoop EcoSystem");
+		    jsonObj.put("description", "Hadoop Description");
+		    jsonObj.put("id", "Hadoop");
+		    
+		    assertEquals(jsonObj.toString(), response.toString());
+
+			
+		} catch (IOException e) {
+			System.out.println("IO Exception Caught ");
+		} catch (JSONException e) {
+			System.out.println("JSONException Caught");
 		}	
 		
 		
