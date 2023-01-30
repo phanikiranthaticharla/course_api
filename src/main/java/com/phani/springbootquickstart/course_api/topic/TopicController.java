d197bdd7b93accd52525c4fc0411c545344d0627
PHANI KIRAN THATICHARLA <phanikiranthaticharla@PHANIs-MBP.attlocal.net>
2023-01-29 22:33:58 -0500

    Add ThreadPoolExecutor and use DeferredResult to process concurrent requests

--- src/main/java/com/phani/springbootquickstart/course_api/topic/TopicController.java
+++ src/main/java/com/phani/springbootquickstart/course_api/topic/TopicController.java
@@ -2,23 +2,34 @@ package com.phani.springbootquickstart.course_api.topic;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 
 @RestController
 public class TopicController {
-	
+
+	private final TaskExecutor taskExecutor;
+
+	public TopicController(TaskExecutor taskExecutor) {
+		this.taskExecutor = taskExecutor;
+	}
 	@Autowired
 	private TopicService topicService; 
 	
 	
 	@GetMapping("/topics")
-	public List<Topic> listAllTopics() {
-		return topicService.getAllTopics();
-				
+	public DeferredResult<List<Topic>> listAllTopics() {
+		DeferredResult<List<Topic>> deferredResult = new DeferredResult<>();
+		taskExecutor.execute(() -> {
+			List<Topic> data = topicService.getAllTopics();
+			deferredResult.setResult(data);
+		});
+		return deferredResult;
 	}
 	
 	@GetMapping("/topics/{topicId}")
